9ac7fed5fd72172dcd6ad0cc801aa9c6721be144
Isaac Avila-Branson <isaacbranson8@gmail.com>
2025-08-27 09:21:22 -0700

    Lab 1 parts 1 2 3

---
+++ product_of_two_numbers.cpp
@@ -0,0 +1,24 @@
+//@author: Isaac Avila-Branson
+//@date: 08/27/2025
+//@purpose: This program calculates the product of two numbers
+
+
+//include the iostream directive (input/output)
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function/method
+int main() {
+
+int num1 = 5;
+int num2 = 10;
+int product = num1 * num2;
+
+//use the std output for the console and print the product
+std::cout << product << std::endl;
+
+return 0;
+
+}
---
+++ take_square_root.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int realNumber = 16;
+    int rootResult = realNumber;
+
+}
