a9c2777f6a6753ffaf3be70d8bbbc948d7f9648b
Isaac Avila-Branson <isaacbranson8@gmail.com>
2025-09-14 18:30:43 -0400

    lab4_part2_completed

--- Lab4/grade_calculator.cpp
+++ Lab4/grade_calculator.cpp
@@ -1,6 +1,6 @@
 //@author: Isaac Avila-Branson
 //@date: 09/14/2025
-//@purpose: Create a program that calculates the average of three integers
+//@purpose: Create a program that calculates the users grade
 #include <iostream>
 
 using namespace std;
@@ -8,7 +8,6 @@ using namespace std;
 
 
 int main() {
-
     //create integer variable to store user values, create float value for decimal results
     int integer_one, integer_two, integer_three;
     float average_value;
@@ -30,5 +29,22 @@ int main() {
     std::cout << "The average of your three numbers is: " << average_value << 
         std::endl;
 
+    //calculate letter grade with if-else statements
+        if (average_value >= 90) {
+        std::cout << "Letter Grade: A\n";
+    } else if (average_value >= 80) {
+        std::cout << "Letter Grade: B\n";
+    } else if (average_value >= 70) {
+        std::cout << "Letter Grade: C\n";
+    } else if (average_value >= 60) {
+        std::cout << "Letter Grade: D\n";
+    } else {
+        std::cout << "Letter Grade: F\n";
+    }
+
     return 0;
+
+
+
+
 }
