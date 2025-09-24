ca689a9e9ab4f2b4d246bb970a2e11ec03458432
Isaac Avila-Branson~ <icbranson8@gmail.com~>
2025-09-24 16:11:21 -0400

    Sync

--- Lab6/sum_loop.cpp
+++ Lab6/sum_loop.cpp
@@ -1,6 +1,6 @@
 //@author: Isaac Avila-Branson
 //@date: 09/24/2025
-//@purpose: Create a program to make a table of an integer and its square in a for loop
+//@purpose: Create a program to iterate 50 times and sum value of adder
 
 #include <iostream>
 using namespace std;
@@ -9,6 +9,10 @@ int main() {
     int sumOp, adder; //create integers to store values
     sumOp = 0;
     adder = 10;
+    for (sumOp; sumOp < 50; sumOp++) {
+        cout << "Value of sumOp: " << sumOp << endl;
+        adder = sumOp + adder;
+    }
 
 
 
