bcbfe62e504d23a3606e2726a51ae87c5df82a35
Isaac Avila-Branson~ <icbranson8@gmail.com~>
2025-09-24 16:29:44 -0400

    Corrected SumOp

--- Lab6/sum_loop.cpp
+++ Lab6/sum_loop.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 int main() {   
     int sumOp, adder, index; //create integers to store values
-    sumOp = 0;
+    sumOp = 0; //Initialize sumOp and adder in next line
     adder = 10;
 
     for (index = 0; index < 50; index++) {
-        sumOp = sumOp + adder;
-        cout << "Value of sum: " << sumOp << endl;
+        sumOp = sumOp + adder + index; //sum sumOp, adder, and the index and assign 
+        cout << "Value of sum: " << sumOp << endl; //Print current value of sumOp
     }
     return 0;
 
