71cd5467fc148f92cf4b91eb9e1fa21bf6447178
Isaac Avila-Branson~ <icbranson8@gmail.com~>
2025-09-24 15:11:07 -0400

    Lab6 Work

---
+++ Lab6/sum_loop.cpp
@@ -0,0 +1,15 @@
+//@author: Isaac Avila-Branson
+//@date: 09/24/2025
+//@purpose: Create a program to make a table of an integer and its square in a for loop
+
+#include <iostream>
+using namespace std;
+
+int main() {   
+    int sumOp, adder; //create integers to store values
+    sumOp = 0;
+    adder = 10;
+
+
+
+}
