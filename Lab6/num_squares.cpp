0a9e0e3c740722157cfca2d929a58820573af5f6
Isaac Avila-Branson~ <icbranson8@gmail.com~>
2025-09-24 14:27:30 -0400

    Lab6part5start

---
+++ Lab6/num_squares.cpp
@@ -0,0 +1,13 @@
+//@author: Isaac Avila-Branson
+//@date: 09/24/2025
+//@purpose: Create a program to count backwards in for-loop
+
+#include <iostream>
+using namespace std;
+
+int main() {  
+    int minNum = 0; //create integer for minNum
+    int maxNum = 100; //create integer for maxNum
+    for (minNum;)
+
+}
