58f678eeac6458f6a87f6e86ec3e662121d28be2
Isaac Avila-Branson <isaacbranson8@gmail.com>
2025-10-27 12:19:28 -0400

    Finished part 1 & 2 of part1

--- Lab10/grade_arrays.cpp
+++ Lab10/grade_arrays.cpp
@@ -7,5 +7,19 @@
 
 using namespace std;
 int main() { 
-    
+    int sizeOfArray = 10; //intiialize size of array
+    int grade; //create integer to hold user input of each grade
+    float averageGrade = 0; //initialize a float to calculate the average grade
+    int gradeBook[sizeOfArray]; //Assign size of gradebook
+    for (int i = 0; i < sizeOfArray; i++) {
+        cout << "Please Enter Grade: ";
+        cin >> grade;
+        gradeBook[i] = grade;
+    }
+   //Next array is used for calculating the average of the grade
+    for (int j = 0; j < sizeOfArray; j++) {
+        averageGrade = gradeBook[j] + averageGrade;
+    }
+    averageGrade = averageGrade / 10;
+    cout << "The average of the grades is: " << averageGrade << endl;
 }
