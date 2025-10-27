722f591dc7522d5dc7d0cf5ad2622b63706ade99
Isaac Avila-Branson <isaacbranson8@gmail.com>
2025-10-27 11:42:19 -0400

    Started Lab10

---
+++ Lab10/grade_arrays.cpp
@@ -0,0 +1,11 @@
+//@author: Isaac Avila-Branson
+//@date: 10/27/2025
+//@purpose: Create a program to take user input and calculate the average grade in an array
+
+#include <iostream>
+
+
+using namespace std;
+int main() { 
+    
+}
